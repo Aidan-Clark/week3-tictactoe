a0e040405ef875c9fd6d57979fe0866debe573d3
unknown <AClark@spartaglobal.com>
2018-12-13 22:02:43 +0000

    Outputs win/draw to screen, prevents overwriting tiles

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(function() {
   var $tds = $("td");
   var $reset = $("#reset");
   var $turnText = $(".playerTurn");
+  var finished = false;
   var solutions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -16,52 +17,53 @@ $(function() {
     [2, 4, 6]
   ];
 
-  console.log(solutions.length)
-
   function tdClick() {
-    $(this).addClass(turn);
-    $(this).html(turn);
-    checkSolution();
+    if ($(this).hasClass("filled") == false && finished == false) {
+      $(this).addClass(turn);
+      $(this).addClass("filled")
+      $(this).html(turn);
+      checkSolution();
 
-    if (turn == "X") {
-      turn = "O"
-    } else {
-      turn = "X"
+      if (turn == "X") {
+        turn = "O"
+      } else {
+        turn = "X"
+      }
+
+      if (finished == false) {
+        $turnText.html("It is " + turn + "'s turn");
+      }
+
     }
-
-    $turnText.html("It is " + turn + "'s turn")
   }
 
   function resetClick() {
     $tds.removeClass("X");
     $tds.removeClass("O");
+    $tds.removeClass("filled");
     $tds.html("");
     turn = "X";
+    finished = false;
     $turnText.html("It is X's turn")
   }
 
   function checkSolution() {
     for (i = 0; i < solutions.length; i++) {
       solution = solutions[i];
-
       if (
         $tds.eq(solution[0]).html() == turn &&
         $tds.eq(solution[1]).html() == turn &&
         $tds.eq(solution[2]).html() == turn
       ) {
-        console.log(turn + " Wins!")
+        finished = true;
+        $turnText.html(turn + " Wins!");
       }
     }
-
-
-    // if (
-    //   $tds.eq(0).html() == turn &&
-    //   $tds.eq(1).html() == turn &&
-    //   $tds.eq(2).html() == turn
-    // ) {
-    //   console.log(turn + " won")
-    // }
-
+    var $filledTiles = $(".filled");
+    if ($filledTiles.length == 9 && finished == false) {
+      $turnText.html("It's a Draw!");
+      finished = true;
+    }
 
   }
 
